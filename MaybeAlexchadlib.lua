435f8b96c78548593d51693025f23c54a602d0cd
Alexchad-source <dudakalexander181112@gmail.com>
2025-07-15 15:07:30 +0200

    Update MaybeAlexchadlib.lua

--- MaybeAlexchadlib.lua
+++ MaybeAlexchadlib.lua
@@ -15,10 +15,6 @@ local Player = Players.LocalPlayer
 local Mouse = Player:GetMouse()
 local UI_NAME = "Alexchad_BroFinally"
 
-if Players.LocalPlayer.PlayerGui:WaitForChild("Alexchad_BroFinally") then
-    Players.LocalPlayer.PlayerGui.Alexchad_BroFinally:Destroy()
-end
-
 local AlexchadGui = Instance.new("ScreenGui")
 AlexchadGui.Name = UI_NAME
 AlexchadGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
